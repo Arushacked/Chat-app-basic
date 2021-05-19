aaf351b437ac5f09cc98b13e52ac99f4956a6eda
Arushacked <46495591+Arushacked@users.noreply.github.com>
2021-05-19 16:06:46 +0530

    Init commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const http = require("http")
+const express = require("express")
+const path = require("path")
+const app = express()
+const socketio = require("socket.io")
+
+const server = http.createServer(app)
+const io = socketio(server)
+
+
+const PORT = process.env.PORT || 3000
+publicDirectoryPath = path.join(__dirname, "../public")
+
+app.use(express.static(publicDirectoryPath))
+
+io.on("connection", (socket)=>{
+    console.log("New Websocket connection established")
+    socket.emit("message", "Welcome!") 
+
+    socket.on("sendMessage", (message)=>{
+        io.emit("message", message)
+    })
+
+})
+
+
+
+server.listen(PORT, ()=>{
+    console.log(`App is up and running at ${PORT}`)
+})
